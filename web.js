7e2b700aaa1c9042f11200d64678a8f51b848952
Bill <brbatwork@gmail.com>
2013-07-08 02:04:17 +0000

    Updated web.js to read from disk

--- web.js
+++ web.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  fs.readFile('index.html', function(err, data) {
+    if (err) throw err;
+    var buf = new Buffer(data);
+    response.send(buf.toString());
+  });
   response.send('Hello World 2!');
 });
 
